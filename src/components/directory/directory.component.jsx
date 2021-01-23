f889abcad575d7d4f3ef628d20eb7e731245878d
VishnuSivadasa1994 <vishnushivadas2007@gmail.com>
2021-01-23 00:49:52 -0500

    homepage components added

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import MenuItem from '../menu-item/menu-item.component';
+import './directory.styles.scss';
+
+class Directory extends React.Component {
+    constructor(){
+        super();
+
+        this.state = {
+            sections: [
+                {
+                  title: 'PAINTINGS',
+                  imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                  id: 1,
+                  linkUrl: 'shop/paintings'
+                },
+                {
+                  title: 'CONCEPT-ARTS',
+                  imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                  id: 2,
+                  linkUrl: 'shop/concept-arts'
+                },
+                {
+                  title: 'LINE-ARTS',
+                  imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                  id: 3,
+                  linkUrl: 'shop/line-arts'
+                },
+                {
+                  title: 'ANIME',
+                  imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                  size: 'large',
+                  id: 4,
+                  linkUrl: 'shop/anime'
+                },
+                {
+                  title: 'SHADING',
+                  imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                  size: 'large',
+                  id: 5,
+                  linkUrl: 'shop/shading'
+                }
+              ]
+        }
+    }
+
+
+
+    render() {
+        return (
+            <div className='directory-menu'>
+                {
+                    this.state.sections.map(({title, imageUrl, id, size }) => (
+                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                    ))
+                }
+            </div>
+        );
+    }
+
+
+
+}
+
+export default Directory;
