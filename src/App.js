ab508a096aacfb8b447c315af04cf0deb2b2d073
VishnuSivadasa1994 <vishnushivadas2007@gmail.com>
2021-01-25 16:10:47 -0500

    added listing in shop

--- src/App.js
+++ src/App.js
@@ -2,19 +2,15 @@ import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import { Switch, Route } from "react-router-dom";
+import ShopPage from './pages/shop/shop.component';
 
 
-const AnimePage = () => (
-  <div>
-    <h1>ANIMEEEEEE</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
       <Route exact={true} path='/' component={Homepage} />
-      <Route path='/anime' component={AnimePage} />
+      <Route path='/shop' component={ShopPage} />
     </div>
   );
 }
