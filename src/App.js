c18ef7812ade1d88c678cdce3f6a95c0003b8cd6
VishnuSivadasa1994 <vishnushivadas2007@gmail.com>
2021-01-24 19:40:08 -0500

    modify

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+import { Switch, Route } from "react-router-dom";
 
+
+const AnimePage = () => (
+  <div>
+    <h1>ANIMEEEEEE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Route exact={true} path='/' component={Homepage} />
+      <Route path='/anime' component={AnimePage} />
     </div>
   );
 }
